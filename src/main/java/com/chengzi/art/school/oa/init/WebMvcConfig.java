ba5603453b0652a182ff5a1f486e7aa6a1b20579
wei.zhou <wei.zhou@17zuoye.com>
2018-12-24 17:52:38 +0800

    first commit

---
+++ src/main/java/com/chengzi/art/school/oa/init/WebMvcConfig.java
@@ -0,0 +1,32 @@
+package com.chengzi.art.school.oa.init;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * Created by zhouwei on 2018/12/24
+ **/
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/webapp/static/");
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/webapp/static/favicon.ico");
+        registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/webapp/templates/");
+    }
+
+}
