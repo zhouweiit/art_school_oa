db4725c4d498bdb2a918efd5a82e092a8f9b3faf
wei.zhou <wei.zhou@17zuoye.com>
2018-12-29 11:42:04 +0800

    调整代码结构

--- src/main/java/com/chengzi/art/school/oa/controller/front/IndexController.java
+++ src/main/java/com/chengzi/art/school/oa/controller/front/IndexController.java
@@ -1,7 +1,12 @@
 package com.chengzi.art.school.oa.controller.front;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by zhouwei on 2018/12/29
  **/
+@Controller
+@RequestMapping("/front/index")
 public class IndexController {
 }
