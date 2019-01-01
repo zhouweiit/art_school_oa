b48bbd124766c29cab993bcdc2134f957fef0f75
Vile Chow <zhouwei_work@163.com>
2019-01-01 23:26:16 +0800

    Merge branch 'master' of github.com:zhouweiit/art_school_oa
    # Conflicts:
    #	src/main/java/com/chengzi/art/school/oa/controller/admin/DemoController.java

--- src/main/java/com/chengzi/art/school/oa/controller/admin/DemoController.java
+++ src/main/java/com/chengzi/art/school/oa/controller/admin/DemoController.java
@@ -1,13 +1,13 @@
 package com.chengzi.art.school.oa.controller.admin;
 
 import com.chengzi.art.school.oa.dto.TableParamDto;
-import com.chengzi.art.school.oa.dto.TeacherDto;
 import com.chengzi.art.school.oa.util.JsonUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-import javax.servlet.http.HttpServletRequest;
 
 import java.util.ArrayList;
 import java.util.HashMap;
